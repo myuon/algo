10e0c226318ec395cb38a3d6ffe1ad93fe195b30
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-02 18:43:43 +0900

    feat: array with size and IO monad laws

--- src/IO.thy
+++ src/IO.thy
@@ -19,7 +19,7 @@ definition empty_world :: "world" where
   "empty_world = \<lparr> memory = (\<lambda>_. undefined), used = 0 \<rparr>"
 
 datatype 'a ref = Ref addr
-datatype 'a array = Array addr
+datatype 'a array = Array addr nat
 
 datatype 'a io = IO (run_io: "(world, 'a) state")
 
@@ -40,7 +40,7 @@ fun new_world :: "'a::repr \<Rightarrow> world \<Rightarrow> 'a ref \<times> wor
 
 fun alloc_world :: "nat \<Rightarrow> world \<Rightarrow> 'a array \<times> world" where
   "alloc_world s w = (
-    ( Array (used w), \<lparr> memory = memory w, used = used w + s \<rparr> )
+    ( Array (used w) s, \<lparr> memory = memory w, used = used w + s \<rparr> )
   )"
 
 fun get_world :: "addr \<Rightarrow> world \<Rightarrow> 'a::repr \<times> world" where
@@ -66,10 +66,10 @@ fun new_array :: "nat \<Rightarrow> ('a array) io" where
   "new_array s = IO (State (alloc_world s))"
 
 fun read_array :: "'a array \<Rightarrow> nat \<Rightarrow> 'a::repr io" where
-  "read_array (Array arr) i = IO (State (get_world (arr + i)))"
+  "read_array (Array arr _) i = IO (State (get_world (arr + i)))"
 
 fun write_array :: "'a array \<Rightarrow> nat \<Rightarrow> 'a::repr \<Rightarrow> unit io" where
-  "write_array (Array arr) i val = IO (State (put_world (arr + i) val))"
+  "write_array (Array arr _) i val = IO (State (put_world (arr + i) val))"
 
 fun swap_array :: "'a::repr array \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> unit io" where
   "swap_array arr i j = do {
@@ -85,6 +85,10 @@ fun swap_array :: "'a::repr array \<Rightarrow> nat \<Rightarrow> nat \<Rightarr
 fun forMu :: "'a list \<Rightarrow> ('a \<Rightarrow> unit io) \<Rightarrow> unit io" where
   "forMu [] f = return ()"
 | "forMu (x#xs) f = f x \<bind> (\<lambda>_. forMu xs f)"
+
+fun mapM :: "('a \<Rightarrow> 'b io) \<Rightarrow> 'a list \<Rightarrow> 'b list io" where
+  "mapM f [] = return []"
+| "mapM f (x#xs) = f x \<bind> (\<lambda>r. mapM f xs \<bind> (\<lambda>rs. return (r#rs)))"
 
 fun whenu :: "bool \<Rightarrow> unit io \<Rightarrow> unit io" where
   "whenu cond f = (if cond then f else return ())"
@@ -107,6 +111,23 @@ code_printing
   | constant write_array \<rightharpoonup> (Haskell) "unsafeWrite"
   | constant swap_array \<rightharpoonup> (Haskell) "unsafeSwap"
   | constant forMu \<rightharpoonup> (Haskell) "forM_"
+  | constant mapM \<rightharpoonup> (Haskell) "mapM_"
   | constant whenu \<rightharpoonup> (Haskell) "when"
+
+(* IO monad *)
+
+lemma monad_left_id: "return a \<bind> f = f a"
+  apply (simp add: return_def bind_def comp_def)
+  done
+
+lemma monad_right_id: "m \<bind> return = m"
+  apply (simp add: return_def bind_def comp_def)
+  done
+
+lemma monad_assoc:
+  fixes m :: "'a io"
+  shows "(m \<bind> f) \<bind> g = m \<bind> (\<lambda>x. f x \<bind> g)"
+  apply (simp add: return_def bind_def comp_def)
+  done
 
 end
