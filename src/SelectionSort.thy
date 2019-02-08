6ad75b45b2159fe3b1ee93499baf3da74afe274c
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-09 01:25:48 +0900

    refactor: selection_sort algorithm

--- src/SelectionSort.thy
+++ src/SelectionSort.thy
@@ -1,23 +1,20 @@
 theory SelectionSort
-  imports Main IOArray
+  imports Main MVector
 begin
 
-fun selection_sort :: "nat io_array \<Rightarrow> unit io" where
-  "selection_sort arr = (let n = get_size arr in forMu (map nat [1..int n]) (\<lambda>i. do {
+fun selection_sort :: "nat mvector \<Rightarrow> unit io" where
+  "selection_sort arr = (let n = size_of_mvector arr in forMu [0..<n] (\<lambda>i. do {
     min_ref \<leftarrow> ref i;
-    forMu (map nat [int i+1..int n]) (\<lambda>j. do {
-      valJ \<leftarrow> read_array arr j;
+    forMu [i..<n] (\<lambda>j. do {
+      valJ \<leftarrow> read arr (j+1);
       m \<leftarrow> ! min_ref;
-      valMin \<leftarrow> read_array arr m;
+      valMin \<leftarrow> read arr m;
       whenu (valJ < valMin) (min_ref := j)
     });
     m \<leftarrow> ! min_ref;
-    swap_array arr i m;
+    swap arr i m;
     return ()
   }))"
-
-export_code selection_sort in Haskell
-  module_name SelectionSort file "gen/"
 
 primrec sorted :: "'a::ord list \<Rightarrow> bool" where
   "sorted [] = True"
@@ -25,13 +22,13 @@ primrec sorted :: "'a::ord list \<Rightarrow> bool" where
 
 fun selection_sort_program where
   "selection_sort_program xs = do {
-    arr \<leftarrow> new_array_from_list xs;
+    arr \<leftarrow> from_list xs;
     selection_sort arr;
-    read_array_as_list arr
+    to_list arr
   }"
 
 theorem selection_sort_is_sorted:
-  assumes "effect (run_io (selection_sort_program xs)) h h' rs"
+  assumes "effect (selection_sort_program xs) h h' rs"
   shows "sorted rs"
   sorry
 
