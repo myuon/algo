93f84e1c17b304475c7f65893d4512ecfd7c4bd8
myuon <ioi.joi.koi.loi@gmail.com>
2019-04-18 00:36:31 +0900

    feat: priority queue with Data.Set

---
+++ code/priority_queue/set.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- Entry k v should be unique in a queue
+
+data Entry k v = Entry k v
+  deriving (Eq, Ord, Show)
+
+newtype PQueue k v = PQueue { getQueue :: S.Set (Entry k v) }
+
+empty :: PQueue k v
+empty = PQueue S.empty
+
+insert :: (Ord k, Ord v) => k -> v -> PQueue k v -> PQueue k v
+insert k v (PQueue set) = PQueue $ S.insert (Entry k v) set
+
+popMax :: PQueue k v -> Maybe (Entry k v, PQueue k v)
+popMax (PQueue set) = fmap (second PQueue) $ S.maxView set
