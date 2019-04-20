02ec5fe1004f58c75d9d6199c4fd8f912ce1f268
myuon <ioi.joi.koi.loi@gmail.com>
2019-04-20 17:39:17 +0900

    fix: full search dijkstra

--- code/dijkstra/full_search.hs
+++ code/dijkstra/full_search.hs
@@ -7,51 +7,44 @@ import qualified Data.Vector.Unboxed.Mutable as VUM
 import Data.ByteString.Internal (accursedUnutterablePerformIO)
 import System.IO.Unsafe
 
-uread :: VU.Unbox a => Int -> VM.IOVector a -> a
-uread i v = accursedUnutterablePerformIO $ VM.unsafeRead v i
+uread :: VU.Unbox a => Int -> VUM.IOVector a -> a
+uread !i !v = accursedUnutterablePerformIO $ VUM.unsafeRead v i
 
-uwrite :: VU.Unbox a => Int -> a -> VM.IOVector a -> VM.IOVector a
-uwrite i !a v = accursedUnutterablePerformIO $ VM.unsafeWrite v i a >> return v
+uwrite :: VU.Unbox a => Int -> a -> VUM.IOVector a -> VUM.IOVector a
+uwrite !i !a !v =
+  accursedUnutterablePerformIO $ VUM.unsafeWrite v i a >> return v
 
-umodify :: VU.Unbox a => Int -> (a -> a) -> VM.IOVector a -> VM.IOVector a
-umodify i f v =
-  accursedUnutterablePerformIO $ VM.unsafeModify v f i >> return v
+umodify :: VU.Unbox a => Int -> (a -> a) -> VUM.IOVector a -> VUM.IOVector a
+umodify !i !f !v =
+  accursedUnutterablePerformIO $ VUM.unsafeModify v f i >> return v
 
-uthaw :: VU.Unbox a => VU.Vector a -> VM.IOVector a
+uthaw :: VU.Unbox a => VU.Vector a -> VUM.IOVector a
 uthaw = accursedUnutterablePerformIO . VU.unsafeThaw
 
-ufreeze :: VU.Unbox a => VM.IOVector a -> VU.Vector a
+ufreeze :: VU.Unbox a => VUM.IOVector a -> VU.Vector a
 ufreeze = accursedUnutterablePerformIO . VU.unsafeFreeze
 
 dijkstra
   :: (VU.Unbox a, Bounded a, Ord a, Num a)
   => Int  -- ^ nodes
-  -> V.Vector [(Int, a)]  -- ^ edges from i-th node: (end, cost)
+  -> V.Vector (VU.Vector (Int, a))  -- ^ edges from i-th node: (end, cost)
   -> Int  -- ^ start node
   -> VU.Vector a  -- ^ constructed graph
 dijkstra !n !edges !s = ufreeze $ fst $ fix
   ( \f (!costs, !visited) -> case findMinView costs visited of
     (-1) -> (costs, visited)
     n ->
-      let
-        visited' = uwrite n True visited
-        c        = uread n costs
-      in
-        if c <= uread n costs
-          then
-            let es = edges V.! n
-            in  f
-                $  flip (,) visited'
-                $! foldl'
-                     ( \(!cs) (!e, !ec) ->
-                       let c' = c + ec
-                       in  c' `seq` if c' < uread e costs
-                             then uwrite e c' cs
-                             else cs
-                     )
-                     costs
-                     es
-          else f (costs, visited')
+      let visited' = uwrite n True visited
+          c        = uread n costs
+      in  if c <= uread n costs
+            then f $ flip (,) visited' $ VU.foldl'
+              ( \(!cs) (!e, !ec) ->
+                let c' = c + ec
+                in  if c' < uread e costs then uwrite e c' cs else cs
+              )
+              costs
+              (edges V.! n)
+            else f (costs, visited')
   )
   (costs0, visited0)
  where
